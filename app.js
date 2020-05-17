133583dcad443d08a47a0e13f18943ae830c64b9
smopthy <smopthy@gmail.com>
2020-05-17 18:12:43 +0800

    add all

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose') 
+
 const app = express()
 
+mongoose.connect('mongodb://localhost/tood-list' ,  { useNewUrlParser: true , useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error',()=>{
+    console.log('monogofb error' )
+})
+
+db.once('open' , ()=>{
+    console.log ('mongodb connected')
+})
 
 app.get('/' , (req , res) => {
     res.send('it work ')
---
+++ models/todo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema //資料庫綱要 
+const todoSchema = new Schema({
+    name : {
+        type : String ,
+        required : true 
+    }
+})
+
+module.exports = mongoose.model('Todo' , todoSchema )
